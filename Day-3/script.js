9fbf6d25259abd2eab01df0dd11b83ed7e8dff3b
ashishsaxena16108 <ashishsaxena2817@gmail.com>
2025-10-30 15:28:17 +0530

    new file:   Day-3/index.html 	new file:   Day-3/script.js 	new file:   Day-3/style.css

---
+++ Day-3/script.js
@@ -0,0 +1,74 @@
+function Product(name,price,category){
+     this.name=name;
+     this.price=price;
+     this.category=category;
+   }
+function max(...arr){
+    return arr.reduce((a,b)=>{return a.price>b.price?a:b});
+}
+function animationOut(div){
+    div.style.transition = "width 0.5s ease, height 0.5s ease, font-size 0.5s ease";
+    div.style.width = "550px";
+    div.style.height="250px";
+    div.style["font-size"]="20px"
+}
+function animationIn(div){
+    div.style.transition = "width 0.5s ease, height 0.5s ease, font-size 0.5s ease";
+    div.style.width = "500px";
+    div.style.height="190px" 
+    div.style["font-size"]="16px"
+}
+(function(){
+   var arr = [];
+   arr.push(new Product("Pencil",23,"Stationary"));
+   arr.push(new Product("Eraser",19,"Stationary"));
+   arr.push(new Product("Table",200,"Furniture"));
+   arr.push(new Product("Chair",140,"Furniture"));
+   var maxProduct = max.apply(null,arr);
+   Product.prototype.getDiscountedPrice=(percent)=>{
+      return (this.price*percent)/100;
+   }
+   document.body.insertAdjacentHTML('afterbegin',`<h1>The costliest product is ${maxProduct.name} with price ${maxProduct.price}`);
+   arr.forEach((product)=>{
+      var div = document.createElement("div");
+      var h=document.createElement("h1");
+      var p = document.createElement("h2");
+      var c = document.createElement("h3");
+      h.innerText=`Product Name : ${product.name}`;
+      h.addEventListener("mouseover",()=>{
+        h.style.transition="background-color 0.5s ease";
+        h.style.backgroundColor="yellow";
+      });
+      h.addEventListener("mouseout",()=>{
+        h.style.transition="background-color 0.5s ease";
+        h.style.backgroundColor="white";
+      });
+      
+      p.innerText=`Price : ${product.price}`;
+      p.addEventListener("mouseover",()=>{
+        p.style.transition="background-color 0.5s ease";
+        p.style.backgroundColor="yellow";
+      });
+      p.addEventListener("mouseout",()=>{
+        p.style.transition="background-color 0.5s ease";
+        p.style.backgroundColor="white";
+      });
+      c.innerText=`Category : ${product.category}`;
+      c.addEventListener("mouseover",()=>{
+        c.style.transition="background-color 0.5s ease";
+        c.style.backgroundColor="yellow";
+      });
+      c.addEventListener("mouseout",()=>{
+        c.style.transition="background-color 0.5s ease";
+        c.style.backgroundColor="white";
+      });
+      div.append(h,p,c);
+      div.addEventListener("mouseover",()=>{
+        animationOut(div);
+      });
+      div.addEventListener("mouseout",()=>{
+        animationIn(div);
+      });
+      document.body.appendChild(div);
+   });
+})()
